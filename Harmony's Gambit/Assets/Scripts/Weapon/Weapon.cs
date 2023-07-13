91ed186440e45d58dd562184c2f8306844ed0c0f
cueqlp <122250645+cueqlp@users.noreply.github.com>
2023-07-14 00:39:35 +0900

    Weapon
    무기 class 생성 + 기본 무기 Fist

--- Harmony's Gambit/Assets/Scripts/Weapon/Weapon.cs
+++ Harmony's Gambit/Assets/Scripts/Weapon/Weapon.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public abstract class Weapon : MonoBehaviour
 {
+    protected List<GameObject> Range;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    protected int damage;
+    public abstract void Start();
+    public abstract void Update();
+    public abstract bool isEnemyInRange(int x, int y, int direction);
 }
