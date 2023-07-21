7900910c46ae92a8591e6ca534448ffc1eb3816f
Aranch422 <aranzh@yonsei.ac.kr>
2023-07-21 10:01:18 +0900

    door button

--- Harmony's Gambit/Assets/Scripts/Structure/DoorManager.cs
+++ Harmony's Gambit/Assets/Scripts/Structure/DoorManager.cs
@@ -6,17 +6,27 @@ using UnityEngine.TextCore.Text;
 public class DoorManager : MonoBehaviour
 {
     GameObject button1, button2;
+    GameObject slot;
+    DoorOpenButton dob1, dob2;
 
     // Start is called before the first frame update
     void Start()
     {
-        button1 = (GameObject)Instantiate(Resources.Load("Prefabs/Structure/DoorOpenButton"));
-        button2 = (GameObject)Instantiate(Resources.Load("Prefabs/Structure/DoorOpenButton"));
+        dob1 = button1.GetComponent<DoorOpenButton>();
+        dob2 = button2.GetComponent<DoorOpenButton>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (dob1.isPressed && dob2.isPressed )
+        {
+            Open();
+        }
+    }
+    
+    void Open()
+    {
+        // slot blockable  «ÿ¡¶
     }
 }
