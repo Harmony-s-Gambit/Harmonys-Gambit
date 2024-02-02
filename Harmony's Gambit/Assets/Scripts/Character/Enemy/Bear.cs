7fec7c805ac23b4cbabbbc74dbc9623da3e235cb
cueqlp <122250645+cueqlp@users.noreply.github.com>
2024-02-02 13:14:17 +0900

    Update Bear.cs

--- Harmony's Gambit/Assets/Scripts/Character/Enemy/Bear.cs
+++ Harmony's Gambit/Assets/Scripts/Character/Enemy/Bear.cs
@@ -26,6 +26,105 @@ public class Bear : Enemy
         }
         else if (color == COLOR.BLUE)
         {
+<<<<<<< Updated upstream
+=======
+            dontMove = false;
+            direction = DIRECTION.STAY;
+            return GameObject.Find(x + "_" + y);
+        }
+
+        if (dashCount > 4)
+        {
+            if(target.GetComponent<Player>().x == this.x)
+                {
+                if(target.GetComponent<Player>().y > this.y)
+                {
+                    dashDirection = DIRECTION.DOWN;
+                }
+                else
+                {
+                    dashDirection = DIRECTION.UP;
+                }
+                dashChargeTurn = 2;
+                direction = DIRECTION.STAY;
+                return GameObject.Find(x + "_" + y);
+                }
+            else if (target.GetComponent<Player>().y == this.y)
+                {
+                if(target.GetComponent<Player>().x > this.x)
+                {
+                    dashDirection = DIRECTION.RIGHT;
+                }
+                else
+                {
+                    dashDirection = DIRECTION.LEFT;
+                }
+                dashChargeTurn = 2;
+                direction = DIRECTION.STAY;
+                return GameObject.Find(x + "_" + y);
+            }
+        }
+        if(dashChargeTurn != 0)
+        {
+            direction = DIRECTION.STAY;
+            return GameObject.Find(x + "_" + y);
+        }
+
+        GridSlotInfo g = GameObject.Find((x + 1) + "_" + y).GetComponent<GridSlotInfo>();
+        direction = DIRECTION.RIGHT;
+        GridSlotInfo t = GameObject.Find((x - 1) + "_" + y).GetComponent<GridSlotInfo>();
+
+        if (target.GetComponent<Player>().color == COLOR.BLUE)
+        {
+            if (g.blueDistance > t.blueDistance)
+            {
+                g = t;
+                direction = DIRECTION.LEFT;
+            }
+            t = GameObject.Find(x + "_" + (y + 1)).GetComponent<GridSlotInfo>();
+            if (g.blueDistance > t.blueDistance)
+            {
+                g = t;
+                direction = DIRECTION.UP;
+            }
+            t = GameObject.Find(x + "_" + (y - 1)).GetComponent<GridSlotInfo>();
+            if (g.blueDistance > t.blueDistance)
+            {
+                g = t;
+                direction = DIRECTION.DOWN;
+            }
+        }
+        else
+        {
+            if (g.redDistance > t.redDistance)
+            {
+                g = t;
+                direction = DIRECTION.LEFT;
+            }
+            t = GameObject.Find(x + "_" + (y + 1)).GetComponent<GridSlotInfo>();
+            if (g.redDistance > t.redDistance)
+            {
+                g = t;
+                direction = DIRECTION.UP;
+            }
+            t = GameObject.Find(x + "_" + (y - 1)).GetComponent<GridSlotInfo>();
+            if (g.redDistance > t.redDistance)
+            {
+                g = t;
+                direction = DIRECTION.DOWN;
+            }
+        }
+        dashCount++;
+        dontMove = true;
+        return GameObject.Find(g.x + "_" + g.y);
+    }
+
+    public override void changeTarget(COLOR c)
+    {
+        if (c == COLOR.RED)
+        {
+            Debug.Log("Touched");
+>>>>>>> Stashed changes
             target = GameObject.Find("redPlayer(Clone)");
         }
         else if (color == COLOR.RED)
@@ -48,6 +147,7 @@ public class Bear : Enemy
     //곰은 벽같은 거 부수고 다닐것이라서 이렇게 만들었습니다
     DIRECTION toPlayer()
     {
+<<<<<<< Updated upstream
         if (Mathf.Abs(targetP.x - thisEnemy.x) > Mathf.Abs(targetP.y - thisEnemy.y))
         {
             if(targetP.y > thisEnemy.y)
@@ -78,5 +178,41 @@ public class Bear : Enemy
         {
             pattern[0] = toPlayer();
         }
+=======
+        int[] movement = new int[2];
+        switch (dashDirection)
+        {
+            case DIRECTION.DOWN:
+                {
+                    movement[0] = 0;
+                    movement[1] = 1;
+                    break;
+                }
+            case DIRECTION.UP:
+                {
+                    movement[0] = 0;
+                    movement[1] = -1;
+                    break;
+                }
+            case DIRECTION.LEFT:
+                {
+                    movement[0] = -1;
+                    movement[1] = 0;
+                    break;
+                }
+            case DIRECTION.RIGHT:
+                {
+                    movement[0] = 1;
+                    movement[1] = 0;
+                    break;
+                }
+        }
+        for(int i = 0; i < 7; i++)
+        {
+            GameObject movingPoint = GameObject.Find((x + movement[0]*i) + "_" + (y + movement[1]*i));
+            if(movingPoint.tag == "Wall") { }
+        }
+        dashCount = 0;
+>>>>>>> Stashed changes
     }
 }
