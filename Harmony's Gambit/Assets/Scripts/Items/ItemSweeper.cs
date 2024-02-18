b2db168c8f1f16f15d4824edff53b1895e952a6d
NeverForgetD <0327max@gmail.com>
2024-02-18 10:06:55 +0900

    migrate from ItemAnimation (1/2)

---
+++ Harmony's Gambit/Assets/Scripts/Items/ItemSweeper.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ItemSweeper : Item
+{
+    GameObject sweeper;
+    Weapon playerWeapon;
+    SpriteRenderer spriteRenderer;
+    public void GetSweeper()
+    {
+        sweeper = GameObject.Find("bone_5/sweeper");
+        spriteRenderer = sweeper.GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = true;
+
+        DestroyItem();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Player2"))
+        {
+            GetSweeper();
+            Destroy(other.GetComponent<Fist>());
+            other.AddComponent<Sweeper>();
+            playerWeapon = other.GetComponent<Sweeper>();
+            playerWeapon.playerWeapon = true;
+            playerWeapon.equiper = other.gameObject;
+            //weapon.equiper = gameObject; >>> why exist?
+            // 공격 범위 적용
+            //aniamtion 전환
+
+            // playerWeapon.Start();
+
+
+        }
+    }
+}
